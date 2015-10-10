300166c5e4b6b5e6aa8d48637732001b40c6d18c
Albannach <zydeco@copacetic.co.uk>
2015-10-10 13:05:59 +0100

    Inclusion of subset data for plotting

--- code/rawData.R
+++ code/rawData.R
@@ -4,8 +4,9 @@ rawData = function (dataFileName) {
     raw$NDate = as.Date(raw$Date, "%d/%m/%Y")
     raw = subset(raw, raw$NDate >= as.Date('01-02-2007', '%d-%m-%Y'))
     raw = subset(raw, raw$NDate < as.Date('03-02-2007', '%d-%m-%Y'))
+    # write.csv(as.matrix(raw[,NDate:=NULL]), file="./data/subset.txt", quote=FALSE)    
     raw$DT = as.POSIXct(paste(raw$Date, raw$Time), format = "%d/%m/%Y %H:%M:%S")
     raw
 }
-
-if (!exists("hpc")) hpc = rawData('./data/household_power_consumption.txt')
+# if (!exists("hpc")) hpc = rawData('./data/household_power_consumption.txt') 
+if (!exists("hpc")) hpc = rawData('./data/subset.txt')
